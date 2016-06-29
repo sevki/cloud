ce71ad98a83c6aed7c6072b95a041a86ce2dbf47
Sevki <s@sevki.org>
2016-06-29 08:09:04 +0300

    mime and status

--- gcs/main.go
+++ gcs/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"mime"
 	"net/http"
 	"os"
+	"path"
+	"runtime"
 	"strings"
+	"time"
 
 	"rsc.io/cloud"
 	"rsc.io/cloud/diskcache"
 	"rsc.io/cloud/google/gcs"
+)
+
+var (
+	start = time.Now()
 )
 
 func main() {
@@ -25,7 +34,33 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fs := http.FileServer(cloud.Dir(cache, strings.TrimPrefix(os.Getenv("BUCKET"), "gs://")))
+	fs := mimeTypeHandler(http.FileServer(cloud.Dir(cache, strings.TrimPrefix(os.Getenv("BUCKET"), "gs://"))))
 	http.Handle("/", fs)
+	http.Handle("/_status", status)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+func mimeTypeHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		mimetype := "application/octet-stream"
+		ext := path.Ext(r.URL.Path)
+		if strings.HasSuffix(r.URL.Path, "/") {
+			ext = ".html"
+		}
+		mt := mime.TypeByExtension(ext)
+		if mt != "" {
+			mimetype = mt
+		}
+
+		w.Header().Set("Content-Type", mimetype)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK\n"))
+	w.Write([]byte(fmt.Sprintf("go:\t%s\n", runtime.Version())))
+	w.Write([]byte(fmt.Sprintf("uptime:\t%s\n", time.Since(start).String())))
+
 }
